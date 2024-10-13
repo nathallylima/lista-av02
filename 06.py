c7f258d1a709d7a9bbd33afb5e5e0d4677fcc885
Nathally Lima <nathallylimas@gmail.com>
2024-10-13 20:07:56 -0300

    commit

---
+++ 06.py
@@ -0,0 +1,18 @@
+## Crie uma função que receba uma string e retorne o número de vogais
+## presentes na string.
+
+def contagem(palavra):
+    vogais = ['a', 'á', 'à', 'â', 'ã', 'e', 'é', 'ê', 'i', 'í', 'o', 'ó', 'ô', 'õ', 'u', 'ú']
+    contagem_vogais = 0
+
+    for letra in palavra.lower():
+        if letra in vogais:
+            contagem_vogais += 1
+    
+    return contagem_vogais
+
+
+
+palavra = input('Digite uma palavra: ')
+resultado = contagem(palavra)
+print(f'O número de vogais na palavra {palavra} é {resultado}')
