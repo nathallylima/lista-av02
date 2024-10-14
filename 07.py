fc2f3793d55db6b22a22b36a94ae6b9cf68cca6e
Nathally Lima <nathallylimas@gmail.com>
2024-10-13 21:07:24 -0300

    commit

---
+++ 07.py
@@ -0,0 +1,15 @@
+def fatorial(n):
+        if n == 0:
+            return 1
+        else:
+            return n * fatorial(n-1)
+while True:
+    try:
+        n = int(input('Digite um número: '))
+        if n < 0:
+              print("Número inválido. Digite um número inteiro positivo.")
+        else:
+            resultado = fatorial(n)
+            print(f'O fatorial de {n} é {resultado}')
+    except ValueError:
+         print('Digite um número válido')
