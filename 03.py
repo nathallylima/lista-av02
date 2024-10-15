846e3f1a13a93d95f3acb8795af0dc49085aecba
Nathally Lima <nathallylimas@gmail.com>
2024-10-15 09:34:50 -0300

    commit

--- 03.py
+++ 03.py
@@ -17,31 +17,28 @@ print("\nCALCULADORA\n")
 
 def calculadora():
     while True:
-        try:
-            operacao = int(input('\nEscolha uma operaçao: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Sair \n'))
+        operacao = int(input('\nEscolha uma operaçao: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Sair \n'))
 
-            if operacao == 5:
-                print("Encerrando o programa...")
-                break
-            elif operacao in [1, 2, 3, 4, 5]:
-                a = int(input('Digite um número: '))
-                b = int(input('Digite outro número: '))
+        if operacao == 5:
+            print("Encerrando o programa...")
+            break
+        elif operacao in [1, 2, 3, 4, 5]:
+            a = int(input('Digite um número: '))
+            b = int(input('Digite outro número: '))
 
-            if operacao == 1:
-                print(f'Soma: {soma(a, b)}')
-            elif operacao == 2:
-                print(f'Subtrcação: {sub(a, b)}')
-            elif operacao == 3:
-                print(f'Multiplicação: {mult(a, b)}')
-            elif operacao == 4:
-                print(f'Divisão: {div(a, b)}')
-            else:
-                print("Opção inválida, digite novamente.")
+        if operacao == 1:
+            print(f'Soma: {soma(a, b)}')
+        elif operacao == 2:
+            print(f'Subtrcação: {sub(a, b)}')
+        elif operacao == 3:
+            print(f'Multiplicação: {mult(a, b)}')
+        elif operacao == 4:
+            print(f'Divisão: {div(a, b)}')
+        else:
+            print("Opção inválida, digite novamente.")
 
-            if operacao == 5:
-                print("Encerrando o programa...")
-                break
-        except ZeroDivisionError:
-            print('Não é possível dividir um número por zero.')
+        if operacao == 5:
+            print("Encerrando o programa...")
+            break
         
 calculadora()
